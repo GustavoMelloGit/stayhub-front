9e8a42ba1d2e6e7b63ca9aec44a61059c55aa27f
Gustavo Marques <gugamello2014@yahoo.com>
2025-09-28 18:33:36 -0300

    feat: add staleTime to useReconcileExternalStays query for improved data freshness

--- src/modules/property/service/PropertyService.hooks.ts
+++ src/modules/property/service/PropertyService.hooks.ts
@@ -37,6 +37,7 @@ export const useReconcileExternalStays = () => {
   } = useQuery({
     queryKey: ['reconcileExternalStays'],
     queryFn: () => PropertyService.reconcileExternalStays(),
+    staleTime: 5 * 60 * 1000, // 5 minutos
   });
 
   return {
