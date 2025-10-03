af4311627d565e410602f1c0f5e54f707305f2c3
Gustavo Marques <gugamello2014@yahoo.com>
2025-10-03 08:58:17 -0300

    refactor: remove nullable deleted_at field from property schema

--- src/modules/property/types/Property.ts
+++ src/modules/property/types/Property.ts
@@ -8,7 +8,6 @@ export const propertySchema = z.object({
   user_id: z.string(),
   created_at: z.coerce.date(),
   updated_at: z.coerce.date(),
-  deleted_at: z.coerce.date().nullable(),
 });
 
 export type Property = z.infer<typeof propertySchema>;
