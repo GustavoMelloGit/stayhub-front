ead5121509a8a044c3117cb2c1e129d7db5f106f
Gustavo Marques <gugamello2014@yahoo.com>
2025-10-05 16:39:30 -0300

    chore: add Prettier and ESLint configuration files, integrate linting and formatting scripts, and set up Husky for pre-commit hooks

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
@@ -14,7 +16,11 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -33,6 +39,7 @@ export default defineConfig([
         },
       ],
       'react-refresh/only-export-components': 'off',
+      'prettier/prettier': 'error',
     },
   },
 ]);
